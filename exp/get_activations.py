ce20b8c43a0b991d5c5521047c5614458938d82d
d0rbu <henryandrecastillo@gmail.com>
2024-04-01 03:57:09 -0500

    fix memory issues and other bugs

--- exp/get_activations.py
+++ exp/get_activations.py
@@ -1,6 +1,7 @@
 import argparse
 import os
 import datasets
+import math
 import torch as th
 from tqdm import tqdm
 from transformers import AutoModel, AutoTokenizer
@@ -14,20 +15,24 @@ def get_activations(
     get_attentions: bool = False,
     get_hidden_states: bool = False,
     dataset: str = "ivanzhouyq/RedPajama-Tiny",
-    model_name: str = "meta-llama/Llama-7b-hf",
+    model_name: str = "meta-llama/Llama-2-7b-hf",
     output_dir: str | None = "out",  # pass None to prevent saving to disk
     return_activations: bool = False,
+    batch_size: int | None = None,
+    truncate_dataset: int = 0,
 ) -> None | tuple[th.Tensor]:
     assert get_attentions or get_hidden_states, "At least one of get_attentions or get_hidden_states must be True"
     assert output_dir is not None or return_activations, "If output_dir is None, return_activations must be True"
 
     if output_dir is not None and get_attentions:
         attn_output_path = os.path.join(output_dir, model_name, dataset, f"{ATTN_PATH}.pt")
+        os.makedirs(os.path.dirname(attn_output_path), exist_ok=True)
     else:
         attn_output_path = None
     
     if output_dir is not None and get_hidden_states:
         hidden_output_path = os.path.join(output_dir, model_name, dataset, f"{HIDDEN_PATH}.pt")
+        os.makedirs(os.path.dirname(hidden_output_path), exist_ok=True)
     else:
         hidden_output_path = None
 
@@ -38,47 +43,70 @@ def get_activations(
     # Load tokenizer
     print("Loading tokenizer...")
     tokenizer = AutoTokenizer.from_pretrained(model_name)
+    tokenizer.add_special_tokens({"pad_token": "[PAD]"})
 
     # Load model
     print("Loading model...")
     model = AutoModel.from_pretrained(
-        model_name, device_map="auto", output_attentions=get_attentions, output_hidden_states=get_hidden_states, return_dict=True
+        model_name, device_map="balanced", output_attentions=get_attentions, output_hidden_states=get_hidden_states, return_dict=True
     )
+    model.resize_token_embeddings(len(tokenizer))
     model.eval()
 
-    # Find largest batch size that works
-    print("Finding optimal batch size...")
-    batch_size = len(dataset)
     with TemporaryDirectory() as tmp_dir, th.no_grad():
-        while True:
+        # Find largest batch size that works
+        print("Finding optimal batch size...")
+        if batch_size is None:
+            batch_size = 256
+        while batch_size > 0:
             try:
+                print(f"Attempting batch size {batch_size}...")
+
                 test_batch = dataset[:batch_size]
-                test_batch = tokenizer(test_batch["text"], padding=True, truncation=True, return_tensors="pt")
+                test_batch = tokenizer(test_batch["text"], padding=True, truncation=True, return_tensors="pt", max_length=model.config.max_position_embeddings)
 
                 th.cuda.empty_cache()
 
                 model(**test_batch)
 
                 break
-            except RuntimeError:
+            except RuntimeError as e:
                 batch_size //= 2
+                print(e)
+        else:
+            raise ValueError("Failed at batch size 0")
+
         del test_batch
+        print(f"Settled on batch size {batch_size}")
 
         # Data loader
         print("Creating data loader...")
         data_loader = th.utils.data.DataLoader(
-            dataset, batch_size=batch_size, collate_fn=datasets.utils.collate_batch, num_workers=4
+            dataset, batch_size=batch_size, num_workers=8,
         )
-        num_batches = len(data_loader)
+        if truncate_dataset <= 0:
+            num_batches = len(data_loader)
+        else:
+            num_batches = math.ceil(truncate_dataset / batch_size)
 
         # Get activations in batches
         print("Getting activations...")
-        attn_tmp_dir = None if attn_output_path is None else os.path.join(tmp_dir, ATTN_PATH)
-        hidden_tmp_dir = None if hidden_output_path is None else os.path.join(tmp_dir, HIDDEN_PATH)
-        os.makedirs(attn_tmp_dir, exist_ok=True)
-        os.makedirs(hidden_tmp_dir, exist_ok=True)
-        for i, batch in tqdm(enumerate(data_loader)):
-            batch = tokenizer(batch["text"], padding=True, truncation=True, return_tensors="pt", return_attention_mask=get_attentions)
+        if attn_output_path is None:
+            attn_tmp_dir = None
+        else:
+            attn_tmp_dir = os.path.join(tmp_dir, ATTN_PATH)
+            os.makedirs(attn_tmp_dir, exist_ok=True)
+
+        if hidden_output_path is None:
+            hidden_tmp_dir = None
+        else:
+            hidden_tmp_dir = os.path.join(tmp_dir, HIDDEN_PATH)
+            os.makedirs(hidden_tmp_dir, exist_ok=True)
+
+        samples = 0
+        for i, batch in tqdm(zip(range(num_batches), data_loader), total=min(len(data_loader), num_batches)):
+            batch = tokenizer(batch["text"], padding=True, truncation=True, return_tensors="pt", max_length=model.config.max_position_embeddings, return_attention_mask=get_attentions)
+            samples += batch["input_ids"].shape[0]
 
             th.cuda.empty_cache()
             outputs = model(**batch)
@@ -92,24 +120,32 @@ def get_activations(
                 padding_mask = padding_mask.expand(model.config.num_attention_heads, -1, -1, padding_mask.shape[-2])  # (H, B, T, T)
 
                 attention_mask = th.logical_and(causal_mask, padding_mask)  # (H, B, T, T)
+                attention_mask = attention_mask.transpose(1, 0,)  # (B, H, T, T)
 
                 flattened_attentions = [attn.cpu()[attention_mask].view(model.config.num_attention_heads, -1) for attn in outputs.attentions]
-                flattened_attentions = th.stack(flattened_attentions, dim=0)  # (L, H, B)
+                flattened_attentions = th.stack(flattened_attentions, dim=0)  # (L, B, H)
 
                 th.save(flattened_attentions, os.path.join(attn_tmp_dir, f"{i}.pt"))
+
+                del causal_mask, padding_mask, attention_mask, flattened_attentions
+
             if get_hidden_states:
                 hidden_states = th.stack([hidden_state.cpu() for hidden_state in outputs.hidden_states])  # (L, B, T, D)
 
                 th.save(hidden_states, os.path.join(hidden_tmp_dir, f"{i}.pt"))
 
-        del data_loader, model, tokenizer, dataset, outputs, batch
+                del hidden_states
+            
+            del outputs
+
+        del data_loader, model, tokenizer, dataset, batch
         th.cuda.empty_cache()
 
         # Collate activations
         print("Collating activations...")
         if get_attentions:
             attn_activations = [th.load(os.path.join(attn_tmp_dir, f"{i}.pt")) for i in range(num_batches)]
-            attn_activations = th.cat(attn_activations, dim=-1)  # (L, H, B)
+            attn_activations = th.cat(attn_activations, dim=1)  # (L, B, H)
 
             if attn_output_path is not None:
                 th.save(attn_activations, attn_output_path)
@@ -134,8 +170,10 @@ if __name__ == "__main__":
     parser.add_argument("--get_attentions", action="store_true")
     parser.add_argument("--get_hidden_states", action="store_true")
     parser.add_argument("--dataset", type=str, default="ivanzhouyq/RedPajama-Tiny")
-    parser.add_argument("--model_name", type=str, default="meta-llama/Llama-7b-hf")
+    parser.add_argument("--model_name", type=str, default="meta-llama/Llama-2-7b-hf")
     parser.add_argument("--output_dir", type=str, default="out")
+    parser.add_argument("--batch_size", type=int, default=None)
+    parser.add_argument("--truncate_dataset", type=int, default=0)
 
     args = parser.parse_args()
 
@@ -145,4 +183,6 @@ if __name__ == "__main__":
         dataset=args.dataset,
         model_name=args.model_name,
         output_dir=args.output_dir,
+        batch_size=args.batch_size,
+        truncate_dataset=args.truncate_dataset,
     )
