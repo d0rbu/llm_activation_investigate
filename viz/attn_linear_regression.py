34804209d948e62f2282f54d0825f8b87f2c9acd
d0rbu <henryandrecastillo@gmail.com>
2024-04-02 19:47:43 -0500

    add truncation option to viz

--- viz/attn_linear_regression.py
+++ viz/attn_linear_regression.py
@@ -12,19 +12,23 @@ def linear_regression_heatmap(
     model_name: str = "meta-llama/Llama-2-7b-hf",
     output_dir: str = "out",
     figures_dir: str = "figures",
+    truncate_data: int = 0,
 ) -> None:
     data_path = os.path.join(output_dir, model_name, dataset, f"{ATTN_PATH}.pt")
     assert os.path.exists(data_path), f"Data path {data_path} does not exist"
 
-    os.makedirs(figures_dir, exist_ok=True)
+    figures_out_dir = os.path.join(figures_dir, model_name, dataset)
+    os.makedirs(figures_out_dir, exist_ok=True)
 
     # Load data
     print("Loading data...")
     flattened_attns = th.load(data_path)  # (L, H, B)
+    if truncate_data > 0:
+        flattened_attns = flattened_attns[..., :truncate_data]
 
     # Heatmap data
-    mse_heatmap = th.zeros(flattened_attns.shape[0] - 1, flattened_attns.shape[0] - 1)
-    r2_heatmap = th.zeros(flattened_attns.shape[0] - 1, flattened_attns.shape[0] - 1)
+    mse_heatmap = th.zeros(flattened_attns.shape[0] - 1, flattened_attns.shape[0])
+    r2_heatmap = th.zeros(flattened_attns.shape[0] - 1, flattened_attns.shape[0])
 
     print("Creating heatmaps...")
     for base_layer, predicted_layer in tqdm(product(range(flattened_attns.shape[0]), repeat=2), total=flattened_attns.shape[0] ** 2):
@@ -51,25 +55,25 @@ def linear_regression_heatmap(
         # Compute MSE
         MSE = residuals.pow(2).mean()
 
-        mse_heatmap[base_layer + 1, predicted_layer - 1] = MSE
-        r2_heatmap[base_layer + 1, predicted_layer - 1] = R2
+        mse_heatmap[base_layer, predicted_layer] = MSE
+        r2_heatmap[base_layer, predicted_layer] = R2
 
     # Plot each heatmap separately and save
     fig, ax = plt.subplots()
-    cax = ax.matshow(mse_heatmap, cmap="RdBu")
+    cax = ax.matshow(mse_heatmap, cmap="RdBu_r")
     fig.colorbar(cax)
     plt.title("MSE Heatmap")
     plt.xlabel("Predicted Layer")
     plt.ylabel("Base Layer")
-    plt.savefig(os.path.join(figures_dir, model_name, dataset, "mse_heatmap.png"))
+    plt.savefig(os.path.join(figures_out_dir, "mse_heatmap.png"))
 
     fig, ax = plt.subplots()
-    cax = ax.matshow(r2_heatmap, cmap="RdBu")
+    cax = ax.matshow(r2_heatmap, cmap="RdBu", vmin=0, vmax=1)
     fig.colorbar(cax)
     plt.title("R^2 Heatmap")
     plt.xlabel("Predicted Layer")
     plt.ylabel("Base Layer")
-    plt.savefig(os.path.join(figures_dir, model_name, dataset, "r2_heatmap.png"))
+    plt.savefig(os.path.join(figures_out_dir, "r2_heatmap.png"))
 
 
 if __name__ == "__main__":
@@ -78,6 +82,7 @@ if __name__ == "__main__":
     parser.add_argument("--model_name", type=str, default="meta-llama/Llama-2-7b-hf")
     parser.add_argument("--output_dir", type=str, default="out")
     parser.add_argument("--figures_dir", type=str, default="figures")
+    parser.add_argument("--truncate_data", type=int, default=0)
 
     args = parser.parse_args()
 
@@ -86,4 +91,5 @@ if __name__ == "__main__":
         model_name=args.model_name,
         output_dir=args.output_dir,
         figures_dir=args.figures_dir,
+        truncate_data=args.truncate_data,
     )
