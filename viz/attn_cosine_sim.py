d4f2bb89d51023e29a8e1a2709bad383b92b0fd6
d0rb <hacperu2010@gmail.com>
2024-04-08 03:30:22 -0500

    cosine sim viz

--- viz/attn_cosine_sim.py
+++ viz/attn_cosine_sim.py
@@ -2,6 +2,7 @@ import os
 import argparse
 import matplotlib.pyplot as plt
 import torch as th
+from typing import Sequence
 from itertools import product
 from tqdm import tqdm
 from exp.get_activations import ATTN_PATH
@@ -13,6 +14,7 @@ def cosine_sim_heatmap(
     output_dir: str = "out",
     figures_dir: str = "figures",
     truncate_data: int = 0,
+    base_layers: Sequence[int] = (0,),
 ) -> None:
     data_path = os.path.join(output_dir, model_name, dataset, f"{ATTN_PATH}.pt")
     assert os.path.exists(data_path), f"Data path {data_path} does not exist"
@@ -23,24 +25,44 @@ def cosine_sim_heatmap(
     # Load data
     print("Loading data...")
     data = th.load(data_path)
-    attn, padding = data["activation"], data["padding"]  # (L, B, H, T, T), (B, T)
+    attn, padding_mask = data["activation"], data["padding"]  # (L, B, H, T, T), (B, T)
     del data
 
     # Heatmap data
-    cosine_sim_heatmap = th.zeros(attn.shape[0], attn.shape[0])
+    cosine_sim_heatmaps = th.zeros(attn.shape[0], attn.shape[0], attn.shape[3])  # (L, L, T)
+    
+    if truncate_data > 0:
+        max_token = min(truncate_data, attn.shape[3])
+        attn = attn[..., :max_token, :max_token]
+        padding_mask = padding_mask[:, :max_token]
+    else:
+        max_token = attn.shape[3]
 
     print("Creating heatmaps...")
-    for base_layer, predicted_layer in tqdm(product(range(attn.shape[0]), repeat=2), total=attn.shape[0] ** 2):
-        pass
+    for base_layer, predicted_layer, token_idx in tqdm(product((base_layers, range(attn.shape[0]), range(max_token))), total=len(base_layers) * attn.shape[0] * max_token):
+        # for each batch that is not a padding token, compute the cosine similarity of average attention across heads
+        base_attn_vectors = attn[base_layer, :, :, token_idx, :token_idx + 1].mean(dim=1)  # (B, token_idx + 1)
+        predicted_attn_vectors = attn[predicted_layer, :, :, token_idx, :token_idx + 1].mean(dim=1)  # (B, token_idx + 1)
 
-    # Plot the heatmap separately and save
-    fig, ax = plt.subplots()
-    cax = ax.matshow(cosine_sim_heatmap, cmap="RdBu_r")
-    fig.colorbar(cax)
-    plt.title("Cosine Similarity Heatmap")
-    plt.xlabel("Layer")
-    plt.ylabel("Layer")
-    plt.savefig(os.path.join(figures_out_dir, "mse_heatmap.png"))
+        # mask padding samples
+        mask = padding_mask[:, token_idx]  # (B,)
+
+        # cosine similarity
+        cosine_sim = th.nn.functional.cosine_similarity(base_attn_vectors, predicted_attn_vectors, dim=-1)  # (B,)
+        cosine_sim = cosine_sim[mask]
+
+        # average cosine similarity
+        cosine_sim_heatmaps[base_layer, predicted_layer, token_idx] = cosine_sim.mean()
+
+    # plot the heatmaps separately and save
+    for base_layer in base_layers:
+        fig, ax = plt.subplots()
+        cax = ax.matshow(cosine_sim_heatmaps[base_layer].T, cmap="RdBu_r")
+        fig.colorbar(cax)
+        plt.title(f"Cosine Similarity Heatmap (Layer {base_layer})")
+        plt.xlabel("Token")
+        plt.ylabel("Layer")
+        plt.savefig(os.path.join(figures_out_dir, f"cosine_sim_heatmap_layer_{base_layer}.png"))
 
 
 if __name__ == "__main__":
