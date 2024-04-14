31b91c30af6d45c65a4dcd77d20e91ea81d34c9d
d0rb <hacperu2010@gmail.com>
2024-04-14 15:43:38 -0500

    start working on perplexity measurement

---
+++ core/skip_attn.py
@@ -0,0 +1,180 @@
+import torch as th
+from transformers import LlamaModel, LlamaTokenizer
+from transformers.models.llama.modeling_llama import logger
+from transformers.cache_utils import StaticCache, DynamicCache, Cache
+from transformers.modeling_outputs import BaseModelOutputWithPast
+from typing import Self, Sequence
+
+
+def is_in_slices(
+    index: int,
+    slices: Sequence[slice],
+    total_length: int,
+) -> bool:
+    for s in slices:
+        start = 0
+        if isinstance(s.start, int):
+            if 0 <= s.start < total_length:
+                start = s.start
+            elif -total_length <= s.start < 0:
+                start = total_length + s.start
+            else:
+                raise ValueError(f"Invalid start index {s.start} for total length {total_length}")
+
+        stop = total_length
+        if isinstance(s.stop, int):
+            if 0 <= s.stop < total_length:
+                stop = s.stop
+            elif -total_length <= s.stop < 0:
+                stop = total_length + s.stop
+            else:
+                raise ValueError(f"Invalid stop index {s.stop} for total length {total_length}")
+        
+        step = s.step if s.step is not None else 1
+
+        if start <= index < stop and (index - start) % step == 0:
+            return True
+
+    return False
+
+
+def convert_to_skip_attn_llama(
+    model: LlamaModel,
+    base_attn_layer: int = 2,
+    predicted_attn_layers: Sequence[slice] = (slice(3, -1)),
+    topk_tokens: int = 8,
+) -> None:
+    # https://github.com/huggingface/transformers/blob/v4.39.3/src/transformers/models/llama/modeling_llama.py#L940
+    def skip_attn_forward(
+        self: Self,
+        input_ids: th.LongTensor = None,
+        attention_mask: th.Tensor | None = None,
+        position_ids: th.LongTensor | None = None,
+        past_key_values: list[th.FloatTensor] | None = None,
+        inputs_embeds: th.FloatTensor | None = None,
+        use_cache: bool | None = None,
+        output_attentions: bool | None = None,
+        output_hidden_states: bool | None = None,
+        return_dict: bool | None = None,
+        cache_position: th.LongTensor | None = None,
+    ) -> tuple | BaseModelOutputWithPast:
+        output_attentions = output_attentions if output_attentions is not None else self.config.output_attentions
+        output_hidden_states = (
+            output_hidden_states if output_hidden_states is not None else self.config.output_hidden_states
+        )
+        use_cache = use_cache if use_cache is not None else self.config.use_cache
+        return_dict = return_dict if return_dict is not None else self.config.use_return_dict
+
+        if (input_ids is None) ^ (inputs_embeds is not None):
+            raise ValueError(
+                "You cannot specify both input_ids and inputs_embeds at the same time, and must specify either one"
+            )
+
+        if self.gradient_checkpointing and self.training and use_cache:
+            logger.warning_once(
+                "`use_cache=True` is incompatible with gradient checkpointing. Setting `use_cache=False`."
+            )
+            use_cache = False
+
+        if inputs_embeds is None:
+            inputs_embeds = self.embed_tokens(input_ids)
+
+        past_seen_tokens = 0
+        if use_cache:  # kept for BC (cache positions)
+            if not isinstance(past_key_values, StaticCache):
+                past_key_values = DynamicCache.from_legacy_cache(past_key_values)
+                past_seen_tokens = past_key_values.get_seq_length()
+
+        if cache_position is None:
+            if isinstance(past_key_values, StaticCache):
+                raise ValueError("cache_position is a required argument when using StaticCache.")
+            cache_position = th.arange(
+                past_seen_tokens, past_seen_tokens + inputs_embeds.shape[1], device=inputs_embeds.device
+            )
+
+        if position_ids is None:
+            position_ids = cache_position.unsqueeze(0)
+
+        causal_mask = self._update_causal_mask(attention_mask, inputs_embeds, cache_position)
+
+        # embed positions
+        hidden_states = inputs_embeds
+
+        # decoder layers
+        all_hidden_states = () if output_hidden_states else None
+        all_self_attns = () if output_attentions else None
+        next_decoder_cache = None
+        top_tokens_mask = None
+
+        for i, decoder_layer in enumerate(self.layers):
+            if output_hidden_states:
+                all_hidden_states += (hidden_states,)
+
+            if self.gradient_checkpointing and self.training:
+                layer_outputs = self._gradient_checkpointing_func(
+                    decoder_layer.__call__,
+                    hidden_states,
+                    causal_mask,
+                    position_ids,
+                    past_key_values,
+                    output_attentions,
+                    use_cache,
+                    cache_position,
+                )
+            else:
+                tokens_mask = top_tokens_mask if is_in_slices(i, predicted_attn_layers, len(self.layers)) else None  # (B, T)
+                attn_mask = causal_mask
+
+                if tokens_mask is not None:
+                    top_token_attn_mask = tokens_mask.unsqueeze(1).unsqueeze(1)  # (B, 1, 1, T)
+                    attn_mask = attn_mask & top_token_attn_mask  # (B, H, T, T)
+
+                layer_outputs = decoder_layer(
+                    hidden_states,
+                    attention_mask=attn_mask,
+                    position_ids=position_ids,
+                    past_key_value=past_key_values,
+                    output_attentions=i == base_attn_layer,
+                    use_cache=use_cache,
+                    cache_position=cache_position,
+                )
+
+            if i == base_attn_layer:
+                token_attn = layer_outputs[1]  # (B, H, T, T)
+                token_attn = token_attn.sum(dim=-1).sum(dim=1)  # (B, T)  how much each token is paid attention, in aggregate, to across heads
+
+                k = min(topk_tokens, token_attn.shape[-1])
+                top_tokens = token_attn.topk(k, dim=-1).indices  # (B, topk)
+                top_tokens_mask = th.full_like(token_attn, float("-inf"))  # (B, T)
+                top_tokens_mask.scatter_(-1, top_tokens, 0)  # (B, T), top tokens are 0, rest are -inf
+
+            hidden_states = layer_outputs[0]
+
+            if use_cache:
+                next_decoder_cache = layer_outputs[2 if output_attentions else 1]
+
+            if output_attentions:
+                all_self_attns += (layer_outputs[1],)
+
+        hidden_states = self.norm(hidden_states)
+
+        # add hidden states from the last decoder layer
+        if output_hidden_states:
+            all_hidden_states += (hidden_states,)
+
+        next_cache = None
+        if use_cache:
+            next_cache = (
+                next_decoder_cache.to_legacy_cache() if isinstance(next_decoder_cache, Cache) else next_decoder_cache
+            )
+        if not return_dict:
+            return tuple(v for v in [hidden_states, next_cache, all_hidden_states, all_self_attns] if v is not None)
+        return BaseModelOutputWithPast(
+            last_hidden_state=hidden_states,
+            past_key_values=next_cache,
+            hidden_states=all_hidden_states,
+            attentions=all_self_attns,
+        )
+
+    bound_forward = skip_attn_forward.__get__(model, model.__class__)
+    setattr(model, "forward", bound_forward)
---
+++ exp/skip_attn_pplx.py
@@ -0,0 +1,168 @@
+import argparse
+import json
+import os
+import math
+import torch as th
+from transformers import AutoModelForCausalLM, AutoTokenizer, LlamaModel
+from tqdm import tqdm
+from typing import Sequence
+from itertools import product
+from core.skip_attn import convert_to_skip_attn_llama
+
+
+SKIP_ATTN_FNS = {
+    LlamaModel: convert_to_skip_attn_llama,
+}
+
+
+def skip_attention_perplexity(
+    model_names: Sequence[str] = ["meta-llama/Llama-2-7b-hf"],
+    datasets: Sequence[str] = ["ivanzhouyq/RedPajama-Tiny"],
+    output_dir: str = "out",
+    batch_size: int | None = None,
+    truncate_dataset: int = 0,
+    max_length: int = 0,
+) -> None:
+    output_location = os.path.join(output_dir, "skip_attn_pplx.json")
+    results = []
+
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+    
+    if os.path.exists(output_location):
+        with open(output_location, "r") as f:
+            results = json.load(f)
+
+    current_model = None
+    current_dataset = None
+    model_batch_sizes = {
+        model_name: None
+        for model_name in model_names
+    }
+    for dataset, model_name in tqdm(product(datasets, model_names), total=len(datasets) * len(model_names), desc="Evaluating models on datasets"):
+        # check if results already exist
+        if any(row["model"] == model_name and row["dataset"] == dataset for row in results):
+            continue
+
+        if current_model != model_name:
+            # load model
+            current_model = model_name
+            model = AutoModelForCausalLM.from_pretrained(model_name)
+
+            # change model forward function to use skip attention
+            if type(model) in SKIP_ATTN_FNS:
+                skip_attn_fn = SKIP_ATTN_FNS[type(model)]
+            else:
+                raise ValueError(f"Model type {type(model)} not supported for skip attention")
+            
+            tokenizer = AutoTokenizer.from_pretrained(model_name)
+            tokenizer.add_special_tokens({"pad_token": "[PAD]"})
+            model.eval()
+
+            if max_length <= 0:
+                max_length = model.config.max_position_embeddings
+        
+        if current_dataset != dataset:
+            # load dataset
+            current_dataset = dataset
+            dataset = datasets.load_dataset(dataset)["train"]
+
+        if model_batch_sizes[model_name] is None:
+            # find largest batch size that works
+            print("Finding optimal batch size...")
+
+            if batch_size is None:
+                model_batch_size = 64
+                while model_batch_size > 0:
+                    try:
+                        print(f"Attempting batch size {model_batch_size}...")
+
+                        test_batch = dataset[:model_batch_size]
+                        test_batch = tokenizer(test_batch["text"], padding=True, truncation=True, return_tensors="pt", max_length=max_length)
+
+                        th.cuda.empty_cache()
+
+                        model(**test_batch)
+
+                        break
+                    except RuntimeError as e:
+                        model_batch_size //= 2
+                        print(e)
+                else:
+                    raise ValueError("Failed at batch size 0")
+
+                del test_batch
+            else:
+                model_batch_size = batch_size
+
+            print(f"Settled on batch size {model_batch_size}")
+            model_batch_sizes[model_name] = model_batch_size
+        else:
+            model_batch_size = model_batch_sizes[model_name]
+
+        # data loader
+        print("Creating data loader...")
+        data_loader = th.utils.data.DataLoader(
+            dataset, batch_size=batch_size, num_workers=8,
+        )
+        if truncate_dataset <= 0:
+            num_batches = len(data_loader)
+        else:
+            num_batches = math.ceil(truncate_dataset / batch_size)
+        
+        result = {
+            "model": model_name,
+            "dataset": dataset,
+            "perplexities": [],
+            "perplexities_skip_attn": [],
+            "loss": [],
+            "loss_skip_attn": [],
+        }
+
+        with th.no_grad():
+            print("Computing vanilla perplexities...")
+            for i, batch in tqdm(enumerate(data_loader), total=num_batches, desc="Computing perplexities"):
+                batch = tokenizer(batch["text"], padding=True, truncation=True, return_tensors="pt", max_length=max_length)
+                outputs = model(**batch)
+                loss = outputs[0]
+                perplexity = th.exp(loss)
+
+                result["perplexities"].extend(perplexity.tolist())
+                result["loss"].extend(loss.tolist())
+            
+            print("Computing skip attention perplexities...")
+            skip_attn_fn(model)  # convert model to use skip attention
+
+            for i, batch in tqdm(enumerate(data_loader), total=num_batches, desc="Computing skip attention perplexities"):
+                batch = tokenizer(batch["text"], padding=True, truncation=True, return_tensors="pt", max_length=max_length)
+                outputs = model(**batch)
+                loss = outputs[0]
+                perplexity = th.exp(loss)
+
+                result["perplexities_skip_attn"].extend(perplexity.tolist())
+                result["loss_skip_attn"].extend(loss.tolist())
+        
+        results.append(result)
+        with open(output_location, "w") as f:
+            json.dump(results, f, indent=4)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--model_names", type=str, nargs="+", default=["meta-llama/Llama-2-7b-hf"])
+    parser.add_argument("--datasets", type=str, nargs="+", default=["ivanzhouyq/RedPajama-Tiny"])
+    parser.add_argument("--output_dir", type=str, default="out")
+    parser.add_argument("--batch_size", type=int, default=None)
+    parser.add_argument("--truncate_dataset", type=int, default=0)
+    parser.add_argument("--max_length", type=int, default=0)
+
+    args = parser.parse_args()
+
+    skip_attention_perplexity(
+        model_names=args.model_names,
+        dataset=args.dataset,
+        output_dir=args.output_dir,
+        batch_size=args.batch_size,
+        truncate_dataset=args.truncate_dataset,
+        max_length=args.max_length,
+    )
